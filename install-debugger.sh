94531ba1f1cd4649d12abf0f3515b3c27a2c45af
David Uhumagho. <uhumaghodavid@gmail.com>
2026-02-20 16:10:46 -0800

    Update debugger install script to v1.0.0

--- install-debugger.sh
+++ install-debugger.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L -o /tmp/ckb-debugger.tar.gz https://github.com/nervosnetwork/ckb-standalone-debugger/releases/download/v0.118.0/ckb-debugger-linux-x64.tar.gz
+curl -L -o /tmp/ckb-debugger.tar.gz https://github.com/nervosnetwork/ckb-standalone-debugger/releases/download/v1.0.0/ckb-debugger-linux-x64.tar.gz
 tar -xzf /tmp/ckb-debugger.tar.gz -C /tmp/
-sudo mv /tmp/ckb-debugger /usr/local/bin/
+sudo mv /tmp/ckb-debugger /usr/local/bin/ckb-debugger
 ckb-debugger --version
