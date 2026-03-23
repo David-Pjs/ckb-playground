55e85b2497041fe73cf4092fc7e39a0fbbce687f
David Uhumagho. <uhumaghodavid@gmail.com>
2026-03-23 08:51:19 -0700

    Skip cluster creation, mint spore into existing cluster

--- projects/spore-protocol/spore.ts
+++ projects/spore-protocol/spore.ts
@@ -1,10 +1,12 @@
-import { createCluster, createSpore, predefinedSporeConfigs } from "@spore-sdk/core";
+import { createSpore, predefinedSporeConfigs } from "@spore-sdk/core";
 import { config as lumosConfig, hd, helpers, RPC } from "@ckb-lumos/lumos";
 import { common } from "@ckb-lumos/common-scripts";
 
 const PRIVATE_KEY = "0x90413b43bbfc86daeec9370e81e77bab" +
   "551a01458058916041237710ffa42d95";
 const SPORE_CONFIG = predefinedSporeConfigs.Aggron4;
+const CLUSTER_ID =
+  "0x37b8b772852d45ed16f8a7a2cd0718c983eda34c0bf40963939f95694f624596";
 
 lumosConfig.initializeConfig(lumosConfig.predefined.AGGRON4);
 
@@ -29,37 +31,14 @@ async function signAndSend(txSkeleton: any): Promise<string> {
 
 async function main() {
   console.log("Address:", address);
+  console.log("Minting spore into cluster:", CLUSTER_ID);
 
-  console.log("\nCreating cluster...");
-  const clusterResult = await createCluster({
-    data: {
-      name: "David Collection",
-      description: "On-chain NFT collection - CKB testnet",
-    },
-    fromInfos: [address],
-    toLock: lock,
-    config: SPORE_CONFIG,
-  });
-
-  const { txSkeleton: clusterTx, outputIndex: cIdx } = clusterResult;
-  const clusterId = clusterTx.get("outputs").get(cIdx).cellOutput.type.args;
-  console.log("Cluster ID:", clusterId);
-
-  const clusterHash = await signAndSend(clusterTx);
-  console.log("Cluster tx:", clusterHash);
-  const base = "https://testnet.explorer.nervos.org/transaction/";
-  console.log("Explorer:", base + clusterHash);
-
-  console.log("\nWaiting 30s for cluster to confirm...");
-  await new Promise((r) => setTimeout(r, 30000));
-
-  console.log("\nMinting spore...");
   const content = Buffer.from("My first on-chain NFT. - David");
   const sporeResult = await createSpore({
     data: {
       contentType: "text/plain",
       content,
-      clusterId,
+      clusterId: CLUSTER_ID,
     },
     fromInfos: [address],
     toLock: lock,
@@ -70,6 +49,7 @@ async function main() {
   const sporeId = sporeTx.get("outputs").get(sIdx).cellOutput.type.args;
   console.log("Spore ID:", sporeId);
 
+  const base = "https://testnet.explorer.nervos.org/transaction/";
   const sporeHash = await signAndSend(sporeTx);
   console.log("Spore tx:", sporeHash);
   console.log("Explorer:", base + sporeHash);
